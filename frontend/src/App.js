5a060b2220c9a2ae8c57c63fb85a91a14dad5612
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-10-21 13:48:55 -0600

    Update App for navbar, route, color theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "components/NavBar/NavBar";
+import Main from "components/Main/Main";
+import "App.css";
 
 function App() {
+  const [lightTheme, setLightTheme] = useState(false);
+  const [userLogin, setUserLogin] = useState(true);
+  const [isUserAdmin, setIsUserAdmin] = useState(true);
+
   return (
-    <div>
-      <NavBar />
+    <div className={lightTheme ? "lightApp" : "darkApp"}>
+      <Router>
+        <NavBar lightTheme={lightTheme} userLogin={userLogin} isUserAdmin={isUserAdmin}/>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
