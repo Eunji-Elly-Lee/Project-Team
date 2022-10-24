4a0eb7af74d0b25a363331e54a339ad7ea7c5681
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2022-10-24 00:09:01 -0600

    Update footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,10 +1,28 @@
+import { Link } from "react-router-dom";
 import "components/Footer/Footer.css";
 
 function Footer({
   lightTheme,
 }) {
   return (
-    <footer></footer>
+    <footer className={lightTheme ? "lightFooter" : "darkFooter"}>
+      <div className="footerRow">
+        <div className="footerLogo">
+          <Link to="/">
+            <img
+              src={lightTheme ? "./assets/logo_black.gif" : "./assets/logo_white.gif"}
+              alt="logo"
+            />
+          </Link>
+        </div>
+        <div className="contactUs">
+          <Link to="#">
+            Contact Us
+          </Link>
+        </div>
+      </div>
+      <p> copyright &copy;2022 Team KillTheCode</p>
+    </footer>
   );
 }
 
