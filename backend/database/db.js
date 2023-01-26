56934bd05129f6f3320eec9543be33bf00a6dde1
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-01-26 12:48:14 -0700

    Create database connection

---
+++ backend/database/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+module.exports = () => {
+  const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  };
+
+  mongoose
+    .connect(process.env.MONGO_URI, connectionParams)
+    .then(() => console.log("Connected to MongoDB successfully"))
+    .catch((err) => console.log(err));
+};
