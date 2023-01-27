fec6e4b082fb894f738d431c674833c568931861
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-01-27 02:05:56 -0700

    Update server for user route

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,9 @@ const port = process.env.PORT || 5000;
 const dotenv = require("dotenv");
 dotenv.config();
 
+// Import routes
+const userRoute = require("./routes/userRoute");
+
 // Database connection
 const connection = require("./database/db");
 connection();
@@ -12,6 +15,8 @@ connection();
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/users", userRoute);
 
 app.listen(port, () => {
   console.log("Server is running on port " + port);
