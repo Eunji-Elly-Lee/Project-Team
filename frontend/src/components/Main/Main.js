115be6abd31ed98ff521c08256b39eeea7366040
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-01-29 00:36:48 -0700

    Update main for theme / user and clean up src code

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,41 +1,60 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { themeActions } from "../../redux/themeSlice";
+import Working from "components/Working/Working";
 import "components/Main/Main.css";
 
 function Main() {
-  const [leftBalloonSrc, setLeftBalloonSrc] = useState("./assets/project_balloon01.png");
-  const [rightBalloonSrc, setRightBalloonSrc] = useState("./assets/about_balloon01.png");
+  const PBSRC01 = "./assets/project_balloon01.png";
+  const PBSRC02 = "./assets/project_balloon02.png";
+  const ABSRC01 = "./assets/about_balloon01.png";
+  const ABSRC02 = "./assets/about_balloon02.png";
+  const [leftBalloonSrc, setLeftBalloonSrc] = useState(PBSRC01);
+  const [rightBalloonSrc, setRightBalloonSrc] = useState(ABSRC01);
   const navigate = useNavigate();
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(themeActions.darkTheme());
+  }, [dispatch]);
 
   return (
-    <div className="mainContainer">
-      <div className="balloonContainer leftBalloon">
-        <img
-          className="balloon"
-          src={leftBalloonSrc}
-          onMouseOver={() => setLeftBalloonSrc("./assets/project_balloon02.png")}
-          onMouseOut={() => setLeftBalloonSrc("./assets/project_balloon01.png")}
-          onClick={() => navigate("/projects")}
-          alt="Kill the code balloon"
-        />
+    <>
+    {user ? (
+      <Working />
+    ) : (
+      <div className="mainContainer">
+        <div className="balloonContainer leftBalloon">
+          <img
+            className="balloon"
+            src={leftBalloonSrc}
+            onMouseOver={() => setLeftBalloonSrc(PBSRC02)}
+            onMouseOut={() => setLeftBalloonSrc(PBSRC01)}
+            onClick={() => navigate("/projects")}
+            alt="Kill the code balloon"
+          />
+        </div>
+        <div className="mainMessage">
+            We are here to <br />
+            <span>create project</span> <br />
+            and keep developing <br />
+            something creative.
+        </div>
+        <div className="balloonContainer">
+          <img
+            className="balloon"
+            src={rightBalloonSrc}
+            onMouseOver={() => setRightBalloonSrc(ABSRC02)}
+            onMouseOut={() => setRightBalloonSrc(ABSRC01)}
+            onClick={() => navigate("/about")}
+            alt="About balloon"
+          />
+        </div>
       </div>
-      <div className="mainMessage">
-          We are here to <br />
-          <span>create project</span> <br />
-          and keep developing <br />
-          something creative.
-      </div>
-      <div className="balloonContainer">
-        <img
-          className="balloon"
-          src={rightBalloonSrc}
-          onMouseOver={() => setRightBalloonSrc("./assets/about_balloon02.png")}
-          onMouseOut={() => setRightBalloonSrc("./assets/about_balloon01.png")}
-          onClick={() => navigate("/about")}
-          alt="Kill the code balloon"
-        />
-      </div>
-    </div>
+    )}
+    </>
   );
 }
 
