94ce447a060bfdb08fef307b2bce2d97656ef897
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-02-03 15:41:06 -0700

    Update App for join us

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "components/NavBar/NavBar";
 import Main from "components/Main/Main";
+import JoinUs from "components/JoinUs/JoinUs";
+import JoinUsVerification from "components/JoinUs/JoinUsVerification";
 import Footer from "components/Footer/Footer";
 import "App.css";
 
@@ -17,6 +19,8 @@ function App() {
         <NavBar lightTheme={theme} userLogin={userLogin} isUserAdmin={isUserAdmin}/>
         <Routes>
           <Route path={`${process.env.PUBLIC_URL}/`} element={<Main />} />
+          <Route basename={process.env.PUBLIC_URL} path="/joinus" element={<JoinUs />} />
+          <Route basename={process.env.PUBLIC_URL} path="/joinus/verification" element={<JoinUsVerification />} />
         </Routes>
         <Footer lightTheme={theme} />
       </Router>
