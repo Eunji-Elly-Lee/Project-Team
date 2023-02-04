350b713206a6591fc6c2f8636e38c8faf29319cd
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-02-04 00:59:04 -0700

    Update join us for redux  and clean up some code

--- frontend/src/components/JoinUs/JoinUs.js
+++ frontend/src/components/JoinUs/JoinUs.js
@@ -1,6 +1,8 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import { themeActions } from "../../redux/themeSlice";
 import { Button, Col, Form, Row } from "react-bootstrap";
 import "components/JoinUs/JoinUs.css";
 
@@ -10,14 +12,16 @@ function JoinUs() {
     email: "",
     password: ""
   };
-  const [user, setUser] = useState(initialUserState);
+  const [newUser, setNewUser] = useState(initialUserState);
   const [message, setMessage] = useState("");
   const [confirmPW, setConfirmPW] = useState("");
   const navigate = useNavigate();
+  const { user } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
 
   const onJoinForm = (e) => {
     const { name, value } = e.target;
-    setUser({ ...user, [name]: value });
+    setNewUser({ ...newUser, [name]: value });
   };
 
   // Submit the form to join
@@ -25,32 +29,31 @@ function JoinUs() {
     e.preventDefault();
 
     // Confirm entered password
-    if (user.password === confirmPW) {
+    if (newUser.password === confirmPW) {
       try {
         // Check if the email is aready registered or not
         const userCheck = await axios.get(
-          `${process.env.REACT_APP_API_URL}/users/user?email=${user.email}`
+          `${process.env.REACT_APP_API_URL}/users/user?email=${newUser.email}`
         )
         .then((response) => {
           return response.data;
         });
   
         if (userCheck) {
-          setMessage("The email is already registered");
+          setMessage("The email is already registered!");
         } else {
           // Send email for verification and get the code from server
           const code = await axios.post(`${process.env.REACT_APP_API_URL}/emails/codeEmail`, {
-            email: user.email
+            email: newUser.email
           })
           .then((response) => {
             return response.data;
           });
 
-          setMessage("");
           // Go to the verification page
           navigate("/joinus/verification", {
             state: {
-              user: user,
+              user: newUser,
               code: code
             }
           });
@@ -63,6 +66,15 @@ function JoinUs() {
       setConfirmPW("");
     }
   };
+
+  useEffect(() => {
+    // If user logins, go to the main page
+    if (user) {
+      navigate("/");
+    } else {
+      dispatch(themeActions.darkTheme());
+    }
+  }, [user, navigate, dispatch]);
 
   return (
     <div className="joinContainer">
@@ -81,7 +93,7 @@ function JoinUs() {
               type="text"
               required
               name="username"
-              value={user.username}
+              value={newUser.username}
               onChange={onJoinForm}
             />
           </Col>
@@ -95,7 +107,7 @@ function JoinUs() {
               type="email"
               required
               name="email"
-              value={user.email}
+              value={newUser.email}
               onChange={onJoinForm}
             />
           </Col>
@@ -109,7 +121,7 @@ function JoinUs() {
               type="password"
               required
               name="password"
-              value={user.password}
+              value={newUser.password}
               onChange={onJoinForm}
             />
           </Col>
