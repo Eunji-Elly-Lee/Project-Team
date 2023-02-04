b8734277abf80f0f5baebf2f1fae2e0a8fda41a8
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-02-04 01:02:26 -0700

    Update App for project component and clean up code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,37 @@
-import { useState } from "react";
 import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import NavBar from "components/NavBar/NavBar";
 import Main from "components/Main/Main";
+import Projects from "components/Projects/Projects";
 import JoinUs from "components/JoinUs/JoinUs";
 import JoinUsVerification from "components/JoinUs/JoinUsVerification";
 import Footer from "components/Footer/Footer";
 import "App.css";
 
 function App() {
-  const [userLogin, setUserLogin] = useState(false);
-  const [isUserAdmin, setIsUserAdmin] = useState(true);  
   const { theme } = useSelector((state) => state.theme);
 
   return (
     <div className={theme ? "lightApp" : "darkApp"}>
       <Router>
-        <NavBar lightTheme={theme} userLogin={userLogin} isUserAdmin={isUserAdmin}/>
+        <NavBar lightTheme={theme} />
         <Routes>
           <Route path={`${process.env.PUBLIC_URL}/`} element={<Main />} />
-          <Route basename={process.env.PUBLIC_URL} path="/joinus" element={<JoinUs />} />
-          <Route basename={process.env.PUBLIC_URL} path="/joinus/verification" element={<JoinUsVerification />} />
+          <Route
+            basename={process.env.PUBLIC_URL}
+            path="/projects"
+            element={<Projects />}
+          />
+          <Route
+            basename={process.env.PUBLIC_URL}
+            path="/joinus"
+            element={<JoinUs />}
+          />
+          <Route
+            basename={process.env.PUBLIC_URL}
+            path="/joinus/verification"
+            element={<JoinUsVerification />}
+          />
         </Routes>
         <Footer lightTheme={theme} />
       </Router>
