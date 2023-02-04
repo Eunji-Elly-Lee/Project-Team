bb5b92bff9c82a899c4a9ab4af0c980a49a56778
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-02-04 00:45:04 -0700

    Update redux store for redux persist

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,24 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import storageSession from "redux-persist/lib/storage/session";
+import { persistReducer } from "redux-persist";
+import thunk from 'redux-thunk';
 import userReducer from "redux/userSlice"
 import themeReducer from "redux/themeSlice"
 
+const reducers = combineReducers({
+  user: userReducer,
+  theme: themeReducer
+});
+
+const persistConfig = {
+  key: "root",
+  storage: storageSession
+}
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-    theme: themeReducer
-  }
+  reducer: persistedReducer,
+  devTools: process.env.NODE_ENV !== 'production',
+  middleware: [thunk],
 });
