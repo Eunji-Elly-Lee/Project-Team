09561548d391b30987e60b3b62fdd06d6b07d080
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-02-04 00:46:22 -0700

    Update index.js for redux persist

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { persistStore } from "redux-persist";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { store } from "redux/store";
 import App from "App";
 import "index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const persistor = persistStore(store);
+
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
