cfc43630999ed2f0affc2dfb7bc1fb65b0bade52
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-02-27 10:11:38 -0700

    Update main - changing usage of working component

--- frontend/src/components/Main/Main.js
+++ frontend/src/components/Main/Main.js
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import { themeActions } from "../../redux/themeSlice";
-import Working from "components/Working/Working";
 import "components/Main/Main.css";
 
 function Main() {
@@ -13,7 +12,6 @@ function Main() {
   const [leftBalloonSrc, setLeftBalloonSrc] = useState(P_B_SRC01);
   const [rightBalloonSrc, setRightBalloonSrc] = useState(A_B_SRC01);
   const navigate = useNavigate();
-  const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -21,41 +19,35 @@ function Main() {
   }, [dispatch]);
 
   return (
-    <>
-    {user ? (
-      // When user logins, show working component
-      <Working />
-    ) : (
-      <div className="mainContainer">
-        <div className="balloonContainer leftBalloon">
-          <img
-            className="balloon"
-            src={leftBalloonSrc}
-            onMouseOver={() => setLeftBalloonSrc(P_B_SRC02)}
-            onMouseOut={() => setLeftBalloonSrc(P_B_SRC01)}
-            onClick={() => navigate("/projects")}
-            alt="Kill the code balloon"
-          />
-        </div>
-        <div className="mainMessage">
-            We are here<br />
-            <span>to create projects</span> <br />
-            and keep developing <br />
-            something creative.
-        </div>
-        <div className="balloonContainer">
-          <img
-            className="balloon"
-            src={rightBalloonSrc}
-            onMouseOver={() => setRightBalloonSrc(A_B_SRC02)}
-            onMouseOut={() => setRightBalloonSrc(A_B_SRC01)}
-            onClick={() => navigate("/about")}
-            alt="About balloon"
-          />
-        </div>
+    <div className="mainContainer">
+      <div className="balloonContainer leftBalloon">
+        <img
+          className="balloon"
+          src={leftBalloonSrc}
+          onMouseOver={() => setLeftBalloonSrc(P_B_SRC02)}
+          onMouseOut={() => setLeftBalloonSrc(P_B_SRC01)}
+          onClick={() => navigate("/projects")}
+          alt="Kill the code balloon"
+        />
       </div>
-    )}
-    </>
+      <div className="mainMessage">
+        We are here
+        <br />
+        <span>to create projects</span> <br />
+        and keep developing <br />
+        something creative.
+      </div>
+      <div className="balloonContainer">
+        <img
+          className="balloon"
+          src={rightBalloonSrc}
+          onMouseOver={() => setRightBalloonSrc(A_B_SRC02)}
+          onMouseOut={() => setRightBalloonSrc(A_B_SRC01)}
+          onClick={() => navigate("/about")}
+          alt="About balloon"
+        />
+      </div>
+    </div>
   );
 }
 
