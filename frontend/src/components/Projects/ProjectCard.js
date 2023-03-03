6d1b69cd06f6a006547fa7e7b8cbbae6a0f880f7
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-03-02 22:56:48 -0700

    Update project card component and css

--- frontend/src/components/Projects/ProjectCard.js
+++ frontend/src/components/Projects/ProjectCard.js
@@ -1,10 +1,52 @@
+import { Button, Card, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "components/Projects/ProjectCard.css";
 
-function ProjectCard() {
+function ProjectCard({ project, login, admin }) {
   return (
-    <div>
-      Project Card!!
-    </div>
+    <Col lg="6" xxl="4">
+      <Card className="projectCard">
+        {/* Only admin can edit the project */}
+        {admin && (
+          <div className="prjEditBtnWrap">
+            <Button
+              className="prjEditBtn"
+              variant="outline-info"
+            >
+              Edit
+            </Button>
+          </div>
+        )}
+        <div>
+          <Card.Body>
+            <Card.Title className="projectTitle">
+              {project.title}
+            </Card.Title>
+            <Card.Text>
+              {project.contents} <br />
+              Used: {project.skills}
+            </Card.Text>
+            {project.github && (
+              <Card.Link href={project.github} target="_blank">
+                Github
+              </Card.Link>
+            )}
+            {project.link && (
+              <Card.Link href={project.link} target="_blank">
+                Link
+              </Card.Link>
+            )}
+            {/* When user logins, show the working link */}
+            {login && (
+              <div className="workingLink">
+                Go to
+                <Link to="/working"> Working</Link>
+              </div>
+            )}
+          </Card.Body>
+        </div>
+      </Card>
+    </Col>
   );
 }
 
