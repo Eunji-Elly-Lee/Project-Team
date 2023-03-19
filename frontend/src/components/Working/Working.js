f0aea81a25c8f3159a51bd03638abfff820615d0
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-03-18 23:19:40 -0600

    Update Working component and css

--- frontend/src/components/Working/Working.js
+++ frontend/src/components/Working/Working.js
@@ -1,9 +1,43 @@
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { themeActions } from "../../redux/themeSlice";
+import { Col, Container, Row } from "react-bootstrap";
+import Calendar from "components/Calendar/Calendar";
+import Chat from "components/Chat/Chat";
 import "components/Working/Working.css";
 
 function Working() {
+  const [projectId, setProjectId] = useState("");
+  const [projectTitle, setProjectTitle] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // Get parameter value from location state and set dark theme
+    if (location.state && location.state.project) {
+      setProjectId(location.state.project._id);
+      setProjectTitle(location.state.project.title);
+      dispatch(themeActions.darkTheme());
+    } else {
+      navigate("/");
+    }
+  }, [location.state, dispatch, navigate]);
+
   return (
-    <div>
-      Working!!
+    <div className="workingContainer">
+      <div className="workingTitle">{projectTitle}</div>
+      <Container>
+        <Row>
+          <Col lg="7">
+            <Calendar projectId={projectId} />
+          </Col>
+          <Col lg="5">
+            <Chat projectId={projectId} />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
