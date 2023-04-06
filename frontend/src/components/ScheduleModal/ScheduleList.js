c0c4d815ed0c7166ecdeacbdce9c0f8aafedd3a5
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-04-06 13:24:16 -0600

    Update schedule list modal and css

--- frontend/src/components/ScheduleModal/ScheduleList.js
+++ frontend/src/components/ScheduleModal/ScheduleList.js
@@ -1,7 +1,17 @@
-import { Modal } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 import "components/ScheduleModal/ScheduleList.css";
 
-function ScheduleList({ show, onHide, schedules }) {
+function ScheduleList({
+  show,
+  onHide,
+  schedules,
+  setCurrentYear,
+  setCurrentMonth
+}) {
+  const onClose = () => {
+    onHide();
+  };
+
   return (
     <Modal
       show={show}
@@ -10,8 +20,52 @@ function ScheduleList({ show, onHide, schedules }) {
       backdrop="static"
       keyboard={false}
       size="lg"
+      className="schListModal"
     >
-      Schedule List!!
+      <Modal.Header className="schListHeader">
+        <Modal.Title className="schListTitle">
+          Schedule List
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="schListBody">
+        <div>
+          Click a schedule to navigate to the calendar for the month of the year.
+        </div>
+        <div className="schListWrap">
+          {schedules.map((schedule) => {
+            const fromInfo =
+              schedule.fromYear + "-" +
+              (schedule.fromMonth < 10 ? "0" + schedule.fromMonth : schedule.fromMonth) + "-" +
+              (schedule.fromDate < 10 ? "0" + schedule.fromDate : schedule.fromDate);
+            
+            const toInfo =
+              schedule.toYear + "-" +
+              (schedule.toMonth < 10 ? "0" + schedule.toMonth : schedule.toMonth) + "-" +
+              (schedule.toDate < 10 ? "0" + schedule.toDate : schedule.toDate);
+            
+            return (
+              <p
+                key={schedule._id}
+                onClick={() => {
+                  setCurrentYear(schedule.fromYear);
+                  setCurrentMonth(schedule.fromMonth - 1);
+                  onHide();
+                }}
+              >
+                {schedule.issue}&nbsp;
+                &#40;{fromInfo}~{toInfo}&#41;
+              </p>
+            );
+          })}
+        </div>
+        <Button
+          className="schListBtn"
+          variant="outline-secondary"
+          onClick={onClose}
+        >
+          Close
+        </Button>
+      </Modal.Body>
     </Modal>
   );
 }
