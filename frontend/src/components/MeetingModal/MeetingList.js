17f576ac36742856f441afc4fe6de84319440fdf
Eunji-Elly-Lee <eunji.lee@edu.sait.ca>
2023-04-07 13:58:32 -0600

    Update meeting list modal and css

--- frontend/src/components/MeetingModal/MeetingList.js
+++ frontend/src/components/MeetingModal/MeetingList.js
@@ -1,7 +1,17 @@
-import { Modal } from "react-bootstrap";
+import { Button, Modal } from "react-bootstrap";
 import "components/MeetingModal/MeetingList.css";
 
-function MeetingList({ show, onHide, meetings }) {
+function MeetingList({
+  show,
+  onHide,
+  meetings,
+  setCurrentYear,
+  setCurrentMonth
+}) {
+  const onClose = () => {
+    onHide();
+  };
+
   return (
     <Modal
       show={show}
@@ -10,8 +20,46 @@ function MeetingList({ show, onHide, meetings }) {
       backdrop="static"
       keyboard={false}
       size="lg"
+      className="mtgListModal"
     >
-      Meeting List!!
+      <Modal.Header className="mtgListHeader">
+        <Modal.Title className="mtgListTitle">
+          Meeting List
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body className="mtgListBody">
+        <div>
+          Click a meeting to navigate to the calendar for the month of the year.
+        </div>
+        <div className="mtgListWrap">
+          {meetings.map((meeting) => {
+            const dateInfo =
+              meeting.meetingYear + "-" +
+              (meeting.meetingMonth < 10 ? "0" + meeting.meetingMonth : meeting.meetingMonth) + "-" +
+              (meeting.meetingDate < 10 ? "0" + meeting.meetingDate : meeting.meetingDate);
+                        
+            return (
+              <p
+                key={meeting._id}
+                onClick={() => {
+                  setCurrentYear(meeting.meetingYear);
+                  setCurrentMonth(meeting.meetingMonth - 1);
+                  onHide();
+                }}
+              >
+                Meeting on {dateInfo}
+              </p>
+            );
+          })}
+        </div>
+        <Button
+          className="mtgListBtn"
+          variant="outline-secondary"
+          onClick={onClose}
+        >
+          Close
+        </Button>
+      </Modal.Body>
     </Modal>
   );
 }
